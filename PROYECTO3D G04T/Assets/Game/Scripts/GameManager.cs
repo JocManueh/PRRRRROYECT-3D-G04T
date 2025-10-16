2667f4932a6a9fcc47027dd5b953720d3985aa99
JocManueh <Jose_manuel.diaz@uao.edu.co>
2025-10-15 20:01:14 -0500

    eee

--- PROYECTO3D G04T/Assets/Game/Scripts/GameManager.cs
+++ PROYECTO3D G04T/Assets/Game/Scripts/GameManager.cs
@@ -8,6 +8,11 @@ public class GameManager : MonoBehaviour
 
     private int score = 0;
     private int ItemsCount = 0;
+
+    public float GlobalTime { get => globalTime; set => globalTime = value; }
+    public int Score { get => score; set => score = value; }
+    public int ItemsCount1 { get => ItemsCount; set => ItemsCount = value; }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
